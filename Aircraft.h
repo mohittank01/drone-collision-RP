b2c2a5a3257c2c5f30fe8fff3eefa8a1ccd89259
Mohit Tank <mohitt13@gmail.com>
2023-03-31 20:04:22 +0100

    Converted into a single class and have a Makefile

---
+++ Aircraft.h
@@ -0,0 +1,48 @@
+#ifndef CLASS_AIRCRAFT
+#define CLASS_AIRCRAFT
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Aircraft{
+    private:
+
+    int callsign;
+    int longitude;
+    int latitude;
+    int altitude;
+    int groundspeed;
+    int index;
+
+
+    string FilePath;
+    int TotalCols;
+    vector<string> AllData;
+    int AllData_size;
+    vector<int> Aircraft_Index;
+    int Aircraft_Index_size;
+    vector<string> Single_Aircraft;
+    int Single_Aircraft_size;
+
+    double* longitude_vector;
+    double* latitude_vector;
+    double* altitude_vector;
+    double* groundspeed_vector;
+
+    void CSVData();
+    void AircraftIndex();
+    void SingleAircraft();
+    void ColumnSelect(int column_no, double* column_pointer);
+
+    void PrintAircraft(vector<string> aircraft, int ldh);
+
+    public:
+    void Set_Parameters_and_Data(string File_Path, int no_cols, int index_input);
+    void Vector_Allocation();
+
+};
+
+
+#endif
